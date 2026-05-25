25e93ba8ade1c2bd75e6939b6f5858bfc104acb0
Isaac Enrique Carmona Blanco <isaaccarmona281@gmail.com>
2026-05-24 21:29:53 -0500

    creacion de la entidad y preparacion para el provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,22 @@
+
+
+
+
+import 'package:aplicacion_chat/domain/entities/message.dart';
+import 'package:flutter/material.dart';
+
+class ChatProvider extends ChangeNotifier{
+
+  List<Message> message = [
+
+    Message(text: 'hola amor',fromWho: FromWho.me),
+    Message(text: 'ya regresaste del trabajo?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async{
+
+  //todo implementar metodo 
+
+  } 
+
+}
